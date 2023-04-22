dfb4fb1cf9b81082173787151600e8ad2e521214
NaitikRathi <84074226+NaitikRathi@users.noreply.github.com>
2023-04-22 19:30:23 +0530

    Bug Fixes #1

--- Assets/Scripts/MenuScripts/BgPara.cs
+++ Assets/Scripts/MenuScripts/BgPara.cs
@@ -26,49 +26,49 @@ public class BgPara : MonoBehaviour
     private void Update()
     {
         //first we see where is the mouse on the screen of the game.
-        var  pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        var  pos = Camera.main.ScreenToViewportPoint(InputManager.Instance.MousePos);
         //I wanted the image to move in the oposite dir of the mouse that's why it is -pos here.
         //this give the effect of the parallax
         transform.position = -pos;
 
         //Then I make this variable basically calculating where the image will be.
-        var _pos_n = new Vector2(_startPos.x - (pos.x/10 * diff), _startPos.y - (pos.y/10 * diff));
+        var _pos_n = new Vector2(_startPos.x - (pos.x * diff), _startPos.y - (pos.y * diff));
 
         //These variable full form is main_x position of the image and main_y positiong of the image.
-        var m_x = 0f;
-        var m_y = 0f;
+        var mX = 0f;
+        var mY = 0f;
 
         //Now we sees that if the image will be outside of the window.
         if (_pos_n.x > Max_pos.x)
         {
-             m_x = Max_pos.x;
+             mX = Max_pos.x;
         }
         // same as before
         else if(_pos_n.x < Min_pos.x)
         {
-            m_x = Min_pos.x;
+            mX = Min_pos.x;
         }
         //If the image will not be outside of the window then the main_x will be _pos_n
         else 
         {
-            m_x = _pos_n.x;
+            mX = _pos_n.x;
         }
 
         //Same thing for the y axis
         if(_pos_n.y > Max_pos.y)
         {
-            m_y = Max_pos.y;
+            mY = Max_pos.y;
         }
         else if (_pos_n.y<Min_pos.y)
         {
-            m_y = Min_pos.y;
+            mY = Min_pos.y;
         }
         else 
         {
-            m_y = _pos_n.y;
+            mY = _pos_n.y;
         }
         
         //Now finally our image will be moved. If this can be optimized, just hit me in my Dm.
-        transform.position = new Vector2(m_x, m_y);
+        transform.position = new Vector2(mX, mY);
     }
 }
