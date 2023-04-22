89ec6a64e8d48b10285cb958e5f78ade64e7304d
NaitikRathi <84074226+NaitikRathi@users.noreply.github.com>
2023-04-22 10:47:20 +0530

    Documentation Done of Bgpara.cs and ButtonsPara.cs
    In this commit  i have done the documentation

--- Assets/Scripts/MenuScripts/ButtonsPara.cs
+++ Assets/Scripts/MenuScripts/ButtonsPara.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ButtonsPara : MonoBehaviour
 {
+    //This works almost exactly as the bg without the if statement because these buttons can't be outside the window. 
+    
     private Vector2 _pos;
     private Vector2 _startPos;
 
@@ -12,12 +14,13 @@ public class ButtonsPara : MonoBehaviour
 
     private void Start()
     {
+        //The start positon will be the position that I have set.
         _startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Just the same code as BgPara.cs
         var pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = pos;
         transform.position = new Vector2(_startPos.x + (pos.x * diff), _startPos.y + (pos.y + diff));
